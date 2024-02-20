5557660637c1da194165908c659304bd3aa87865
asim <asim@aslam.me>
2024-02-20 19:56:18 +0000

    multiple prayer times

--- main.go
+++ main.go
@@ -38,55 +38,74 @@ var (
 `
 )
 
+type City struct {
+	Name string
+	Lat,Lon float64
+	Location string
+}
+
+var cities = []City{
+	City{Name: "London", Lat: 51.41334, Lon: -0.36701, Location: "Europe/London"},
+	City{Name: "Doha", Lat: 25.286106, Lon: 51.534817, Location: "Asia/Qatar"},
+	City{Name: "New York", Lat: 40.730610, Lon: -73.935242, Location: "America/New_York"},
+}
+
 func dateFormat(v time.Time) string {
 	return v.Format(time.Kitchen)
 }
 
-func printSchedule(sched prayer.Schedule) string {
+func printSchedule(city string, sched prayer.Schedule) string {
 	format := func(k string, v time.Time) string {
 		return fmt.Sprintf(`<div><span id="name">%s</span><span id="time">%s</span></div>`, k, dateFormat(v))
 	}
 
-	var str string
+	str := fmt.Sprintf(`<h2 id="%s">%s</h2>`, city, city)
 	str += format("Fajr", sched.Fajr)
 	str += format("🌅", sched.Sunrise)
 	str += format("Zuhr", sched.Zuhr)
 	str += format("Asr", sched.Asr)
 	str += format("Maghrib", sched.Maghrib)
 	str += format("Isha", sched.Isha)
-
+	str += "<br><br>"
 	return str
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	date := time.Now().Format(time.DateOnly)
 
-	// Calculate prayer schedule in London for 2023.
-	// Since London in higher latitude, make sure to enable the adapter.
-	europeLondon, _ := time.LoadLocation("Europe/London")
-	londonSchedules, _ := prayer.Calculate(prayer.Config{
-		Latitude:            51.41334,
-		Longitude:           -0.36701,
-		Timezone:            europeLondon,
-		TwilightConvention:  prayer.MWL(),
-		AsrConvention:       prayer.Shafii,
-		HighLatitudeAdapter: prayer.NearestLatitude(),
-		PreciseToSeconds:    true,
-	}, 2024)
-
 	head := "<p>I bear witness there is no deity but God, and I bear witness that Muhammad is the Messenger of God.</p>"
+	nav := ""
 	content := ""
 
-	for _, sched := range londonSchedules {
-		if sched.Date != date {
-			continue
+	for _, city := range cities {
+		// Calculate prayer schedule in London for 2023.
+		// Since London in higher latitude, make sure to enable the adapter.
+		tz, _ := time.LoadLocation(city.Location)
+		schedules, _ := prayer.Calculate(prayer.Config{
+			Latitude:            city.Lat,
+			Longitude:           city.Lon,
+			Timezone:            tz,
+			TwilightConvention:  prayer.MWL(),
+			AsrConvention:       prayer.Shafii,
+			HighLatitudeAdapter: prayer.NearestLatitude(),
+			PreciseToSeconds:    true,
+		}, 2024)
+
+
+		nav += `<a href="#`+city.Name+`" class="head">`+city.Name+`</a>`
+
+		for _, sched := range schedules {
+			if sched.Date != date {
+				continue
+			}
+
+			content += printSchedule(city.Name, sched)
 		}
-		content += printSchedule(sched)
-		// schedule for today
-		out := mu.Template("Pray", "Islamic Prayer Times", "", fmt.Sprintf(template, head, content))
+	}
+
+		out := mu.Template("Pray", "Islamic Prayer Times", nav, fmt.Sprintf(template, head, content))
 		w.Write([]byte(out))
 		return
-	}
 }
 
 func main() {
